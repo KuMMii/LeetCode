eaf97ea5ec913a31c4e7c0aba854cc9e437b5412
KuMMii <128502524+KuMMii@users.noreply.github.com>
2023-12-09 23:45:37 +0900

    Time: 1 ms (98.35%), Space: 44.5 MB (30.42%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,52 +1,27 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        boolean[] check=new boolean[flowerbed.length];
+       if (n == 0) {
+            return true;
+        }
+        
+        //n>0
         for (int i = 0; i < flowerbed.length; i++) {
-            //1이면 true로 만들거임
-            if(flowerbed[i]==1){
-                //맨끝 애들 예외처리(1개 이상일 때)
-                if (flowerbed.length>1) {
-                    if (i == 0) {
-                        check[0] = true;
-                        check[1] = true;
-                    } else if (i == flowerbed.length - 1) {
-                        check[flowerbed.length - 1] = true;
-                        check[flowerbed.length - 2] = true;
-                    } else {
-                        check[i - 1] = true;
-                        check[i] = true;
-                        check[i + 1] = true;
-                    }
-                }else{
-                    check[i]=true;
+            if (flowerbed[i] == 0) {
+                boolean leftEmptyPot=(i==0)||(flowerbed[i-1]==0);
+                boolean rightEmptyPot=(i== flowerbed.length-1)||(flowerbed[i+1]==0);
+
+                if (leftEmptyPot && rightEmptyPot) {
+                    flowerbed[i]=1;
+                    n--;
                 }
+                
+                if (n==0) return true;
+                    
+                
             }
-        }//for
-
-        for (int i = 0; i < n; i++) {
-            boolean planted=false;
-            int idx=0;
-            while (idx != flowerbed.length) {
-                //false 면 심기
-                if (check[idx] == false) {
-                    check[idx]=true;
-                    if(idx>0) check[idx-1]=true;
-                    if(idx<flowerbed.length-1) check[idx+1]=true;
-
-                    planted=true;
-                    break;
-                }
-
-
-                idx++;
-                if (idx == flowerbed.length && planted == false) {
-                    return false;
-                }
-            }
-
-            
-        }//for
-
-        return true;
+        }
+        
+        return false;
+        
     }
 }

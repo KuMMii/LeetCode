8ce9812a5037d80178329a9320793bbfd1715aa7
KuMMii <128502524+KuMMii@users.noreply.github.com>
2023-12-14 01:36:15 +0900

    Time: 48 ms (13.13%), Space: 49.7 MB (5.37%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,25 +1,33 @@
 class Solution {
     public String reverseVowels(String s) {
-        List<Integer> listForIdx = new ArrayList<>();
-        List<Character> listForChar = new ArrayList<>();
+       StringBuilder sb = new StringBuilder(s);
+        String[] word = new String[s.length()];
+        word = s.split("");
+        String vowels = "aeiouAEIOU";
+        int start = 0;
+        int end = s.length() - 1;
 
-        StringBuilder sb = new StringBuilder(s);
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'a' ||s.charAt(i) == 'e' ||s.charAt(i) == 'i' ||s.charAt(i) == 'o' ||s.charAt(i) == 'u'
-                    || s.charAt(i) =='A' || s.charAt(i) =='E' || s.charAt(i) =='I' ||s.charAt(i) == 'O' || s.charAt(i) =='U') {
-                listForIdx.add(i);
-                listForChar.add(s.charAt(i));
+        while (start < end) {
+            //when the start pointer points vowel
+            if (!vowels.contains(word[start])) {
+                start++;
             }
-        }//for
 
-        //reverse the list of char
-        Collections.reverse(listForChar);
+            //when the end pointer points vowel
+            if (!vowels.contains(word[end])) {
+                end--;
+            }
 
-        for (int i = 0; i < listForIdx.size(); i++) {
-            int idx = listForIdx.get(i);
-            sb.replace(idx, idx+1, listForChar.get(i).toString());
+            if (start < end && vowels.contains(word[start]) && vowels.contains(word[end])) {
+                sb.replace(start, start + 1, word[end]);
+                sb.replace(end, end + 1, word[start]);
+                start++;
+                end--;
+            }
+
         }
+
+        System.out.println(sb.toString());
 
        return sb.toString();
         

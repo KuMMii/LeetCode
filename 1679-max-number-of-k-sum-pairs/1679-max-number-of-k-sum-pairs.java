fa514b8715e19f758fcd762ea1e55c1ae2f2c95c
KuMMii <128502524+KuMMii@users.noreply.github.com>
2023-12-23 22:40:01 +0900

    Time: 39 ms (16.47%), Space: 55.7 MB (42.53%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,23 +1,21 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
+        Map<Integer, Integer> map = new java.util.HashMap<>();
         int cnt = 0;
-
-        int n = nums.length;
-
-        for (int i = 0; i <= n - 2; i++) {
-            if (k > nums[i] && nums[i] != 0) {
-                for (int j = i + 1; j <= n - 1; j++) {
-                    if (nums[i] + nums[j] == k && nums[j] != 0) {
-                        nums[i] = 0;
-                        nums[j] = 0;
-                        cnt++;
-                        break;
-                    }
-                }
-            }
+        for (int i = 0; i < nums.length; i++) {
+            int res = k - nums[i];
+            if (map.containsKey(res)) {
+                cnt++;
+                if (map.get(res) == 1) map.remove(res);
+                else map.put(res, map.get(res) - 1);
+            } else {
+                /*map.getOrDefault(map.get(nums[i]),0)+1
+                this is because if nums[i] is in the map, get that value or put the value as 0 and add 1 to both of them.
+                 */
+                map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            } 
         }
-
-
+        
         return cnt;
     }
 }

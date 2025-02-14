cc0900db98b1414843d0d828334acc83f353d06a
Jieun Kim <128502524+KuMMii@users.noreply.github.com>
2025-02-14 10:37:56 +0900

    Time: 244 ms (67.35%), Space: 20.7 MB (38.36%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        global R, C
+        R, C= len(grid), len(grid[0])
+        global visited 
+        visited = [[False]*C for _ in range(R)]
+        ans=0
+
+        for r in range(R):
+            for c in range(C):
+                if not visited[r][c] and grid[r][c]=="1":
+                    self.BFS(r, c, grid)
+                    ans+=1
+
+        return ans
+    
+    def BFS(self, r: int, c: int, grid: List[List[str]]):
+        row=[-1, 1, 0, 0]
+        col=[0,0,-1,1]
+
+        visited[r][c]=True
+        for i in range(4):
+            nr, nc =r+row[i], c+col[i]
+            if (0<=nr<R and 0<=nc<C) and not visited[nr][nc] and grid[nr][nc]=="1":
+                self.BFS(nr, nc, grid)

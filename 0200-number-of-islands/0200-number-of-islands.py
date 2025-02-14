48171638cfa552580c1a66a5f0008303bdb4db82
Jieun Kim <128502524+KuMMii@users.noreply.github.com>
2025-02-14 10:42:13 +0900

    Time: 265 ms (38.51%), Space: 20.5 MB (39.11%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,5 +1,8 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+
         global R, C
         R, C= len(grid), len(grid[0])
         global visited 
